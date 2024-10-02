1a4015be92bd6c12999a3af440e0848fc05176cb
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-10-03 07:48:05 +0800

    Optimized the Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,134 @@
 
-function Home () {
+function Home() {
 
     return (
-        <>
-            <h1>This is the Home Page</h1>
-        </>
+        <main>
+
+            <section className="bg-blue-200 sm:flex lg:p-16 p-12">
+
+                <div className="basis-1/2">
+
+                    <h1 className="text-4xl lg:text-7xl font-bold my-16">We Treat Your Pet Like It's One of Our Own!</h1>
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 font-bold">
+
+                        <button className="rounded-full bg-blue-300 p-3">Request an appointment</button>
+                        <button className="rounded-full bg-blue-300 p-3">Call +12 345 678 9012</button>
+
+                    </div>
+
+                    <div className="flex gap-2 mt-5">
+                        <button className="rounded-full bg-blue-300 p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 fill-blue-900"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                            </svg>
+                        </button>
+
+                        <button className="rounded-full bg-blue-300 p-3">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 fill-blue-900"
+                                fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                            </svg>
+                        </button>
+
+                    </div>
+
+
+                </div>
+
+                <div className="basis-1/2">
+                    <img className="mx-auto mb-14" src="https://clipart-library.com/2023/img-hero-vet.png" alt="" />
+
+                </div>
+
+
+
+            </section>
+
+            <section className="bg-blue-400 sm:flex p-14">
+
+                <div className="basis-1/2">
+                    <img src="https://images.vexels.com/content/281837/preview/veterinarian-animals-people-a209c2.png" alt="" />
+
+                </div>
+
+                <div className="basis-1/2 mt-10">
+
+                    <h2 className="lg:text-6xl text-4xl font-bold">Quality veterinary care, right here in your neighborhood</h2>
+                    <p className="mt-16 font-light text-md">Serving the pets of the Bronx and Westchester communities since 1982, Broadway Animal Hospital
+                        of Riverdale is one of the few locally owned and operated veterinary practices in the area.
+                        We offer a comprehensive range of veterinary services to address the needs of dogs and cats!
+                        Our skilled and caring medical team is here whether your pet needs wellness care or advanced
+                        medical treatment. We are your neighborhood vet!
+                    </p>
+                    <button className="bg-white rounded-full p-3 mt-10">Our Services</button>
+
+                </div>
+
+            </section>
+
+            <section className="bg-blue-200 sm:flex lg:p-20 p-14">
+
+                <div className="basis-1/2 mt-14">
+                    <h3 className="text-4xl font-semibold">A Personal Approach to Veterinary Care</h3>
+
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-16">
+
+                        <div>
+                            <hr className="border-blue-600" />
+                            <h4 className="font-semibold text-4xl mt-5">Detailed Histories</h4>
+                            <p className="mt-4">We take detailed histories to learn as much as possible about our patients and their problems.</p>
+
+                        </div>
+
+                        <div>
+                            <hr className="border-blue-600" />
+                            <h4 className="font-semibold text-4xl mt-5">Thorough Exams</h4>
+                            <p className="mt-4">We give thorough physical exams to help guide our diagnostic approach.</p>
+
+                        </div>
+
+                        <div>
+                            <hr className="border-blue-600" />
+                            <h4 className="font-semibold text-4xl mt-5">No Extra Tests</h4>
+                            <p className="mt-4">We use appropriate tests selectively to help confirm a diagnosis.</p>
+
+                        </div>
+
+                        <div>
+                            <hr className="border-blue-600"/>
+                            <h4 className="font-semibold text-4xl mt-5">Honest Help</h4>
+                            <p className="mt-4">We are honest and open with our clients about their pet's situation, involving them in the decision-making process.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="basis-1/2">
+
+                    <img className="my-auto mx-auto mt-16" src="https://static.vecteezy.com/system/resources/previews/017/190/037/original/dog-clipart-in-a-cup-png.png" alt="" />
+
+                </div>
+
+            </section>
+
+            <section className="bg-blue-400 p-5">
+
+            </section>
+
+        </main>
     )
 
 }
 
-export default Home
+export default Home
