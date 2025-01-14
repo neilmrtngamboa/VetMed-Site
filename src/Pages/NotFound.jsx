075b0c3c7ba573db05535c8d3f1e947742c29dd1
neilmrtngamboa <nmg.gamboa@gmail.com>
2025-01-14 23:24:18 +0800

    Optimized the 404 page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,9 +2,17 @@
 function NotFound () {
 
     return (
-        <>
-            <h1>This is the NotFound Page</h1>
-        </>
+        <main className="bg-blue-400 py-20">
+            
+            <div className="text-center my-48">
+
+                <h1 className="text-9xl font-extrabold">404</h1>
+                <p className="text-4xl mt-3 font-bold"> Uh Oh! That page doesn't exist.</p>
+
+            </div>
+            
+
+        </main>
     )
 
 }
